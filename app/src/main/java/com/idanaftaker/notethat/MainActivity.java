51fa2215cc7f66f93bc3b661b72838b2623391a2
idanaftaker <idanaftaker@gmail.com>
2016-09-07 12:28:11 +0300

    Adding CRUD methods to the content provider

--- app/src/main/java/com/idanaftaker/notethat/MainActivity.java
+++ app/src/main/java/com/idanaftaker/notethat/MainActivity.java
@@ -1,8 +1,11 @@
 package com.idanaftaker.notethat;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        insertNote("New Note");
+
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        Log.d("Main Activity", "Inserted note " + noteUri.getLastPathSegment());
     }
 }
