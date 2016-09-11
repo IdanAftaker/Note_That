19530bfcbb57dd588ad86cd2f860faa84236516a
idanaftaker <idanaftaker@gmail.com>
2016-09-11 14:32:12 +0300

    Creating the editor activity and layout

--- app/src/main/java/com/idanaftaker/notethat/MainActivity.java
+++ app/src/main/java/com/idanaftaker/notethat/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -13,10 +14,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+    private static final int EDITOR_REQUEST_CODE = 1001;
     private NotesCursorAdapter cursorAdapter;
 
 
@@ -109,5 +112,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         cursorAdapter.swapCursor(null);
+    }
+
+    public void openEditorForNewNote(View view) {
+        Intent intent = new Intent(this, EditorActivity.class);
+        startActivityForResult(intent, EDITOR_REQUEST_CODE);
     }
 }
